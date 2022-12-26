421fe422ea12a80669317267175380fe70eb559c
unknown <marinbarac21@gmail.com>
2022-12-26 17:14:43 +0100

    Created react project

---
+++ multi-step-form/src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import sidebar from './assets/bg-sidebar-desktop.svg';
+
+function App() {
+  return (
+    <main>
+      <img src={sidebar} />
+    </main>
+  );
+}
+
+export default App;
