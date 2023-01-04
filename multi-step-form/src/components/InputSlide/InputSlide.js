69b364f1126324940de04b1a172353d93483b775
unknown <marinbarac21@gmail.com>
2023-01-04 14:57:01 +0100

    Added desktop styles for personal info

--- multi-step-form/src/components/InputSlide/InputSlide.js
+++ multi-step-form/src/components/InputSlide/InputSlide.js
@@ -3,8 +3,10 @@ import styles from './InputSlide.module.scss';
 const InputSlide = ({ input }) => {
     return <div className={`${styles.container} ${styles[input.class]}`}>
         {input.icon && <img src={input.icon} alt={`${input.name} icon`} className={styles.icon}/>}
-        <h1 className={styles.title}>{input.title}</h1>
-        <p className={styles.description}>{input.description}</p>
+        <div className={styles.titleContainer}>
+            <h1 className={styles.title}>{input.title}</h1>
+            <p className={styles.description}>{input.description}</p>
+        </div>
         {input.jsxElement}
     </div>
 }
--- multi-step-form/src/components/Sidebar/Step/FormStep.js
+++ multi-step-form/src/components/Sidebar/Step/FormStep.js
@@ -4,8 +4,8 @@ const FormStep = ({ id, title, index, active}) => {
     return <div className={`${styles.container} ${active ? styles.active : ''}`}>
         <div className={styles.numberContainer}>{index + 1}</div>
         <div className={styles.textContainer}>
-            <h4>{id}</h4>
-            <h3>{title}</h3>
+            <h4 className={styles.id}>{id}</h4>
+            <h3 className={styles.title}>{title}</h3>
         </div>
     </div>
 } 
